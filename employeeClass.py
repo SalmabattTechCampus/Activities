f4a78e8019a94438d38e6c769fce78ca37bf2632
salmabatttechcampus <salma-albattashi@hotmail.com>
2023-11-29 15:29:06 +0400

    7th commit

---
+++ employeeClass.py
@@ -0,0 +1,22 @@
+class Employee:
+    
+    
+     def __init__(self, name, ID, salary, department):
+       self.__name = name
+       self.__ID = ID
+       self.__salary=salary
+       self.__department= department
+       
+       
+    list =["Adams", "E7876", "50000", "ACCOUNTING",
+           "jhones", "E7499", "45000", "RESEARCH",
+           "MARTIN", "E7900", "50000", "SALES"
+           "SMITH", "E7698", "55000"]
+    
+    def caculate_emp_salary
+     overtime = hours_worked -50
+     amount = (overtime*(salary/50))
+     
+     
+     
+    def emp_assign_deartment
---
+++ libraryClass.py
@@ -0,0 +1,29 @@
+#create a library class that contains a list book objects. Include methods to add a book , display all books, and search for book by title.
+
+
+class library:
+    
+    def __init__(self):
+        self.library = []
+    def add_book(self," title", "aurthor","copies"):
+        book = {"Title":title, "Author":, "copies": copies}
+        self.liprary.append(book)
+        
+    def display_books(self):
+        for i in self.library:
+            print(i)
+            
+    def search_book(self,title):
+        for i in self.library:
+            if(i["Title"] == title)
+            print(i[Author])
+            
+lb1 = library()
+lb1.add_book("python", "A", 4)
+lb1.add_book("C++","B",2)
+lb1.disply_books()
+lb1.search_book("Python")
+        
+        
+    
+    
